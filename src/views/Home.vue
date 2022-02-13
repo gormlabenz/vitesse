fa92a70300a7474e08a416d6d097cfe016729ced
gormlabenz <gorm-labenz@hotmail.com>
2022-02-13 16:16:32 +0100

    removed Footer added styling

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,6 @@
 <template>
-  <main class="px-4 py-10 text-center text-gray-700 dark:text-gray-200">
-    <Footer />
-    <div class="mt-5 mx-auto text-center opacity-25 text-sm">
-      <h1>Hello World</h1>
+    <div class="mt-5 mx-auto text-center ">
+      <h1 class="text-4xl font-bold text-gray-500">Hello World</h1>
+      <counter />
     </div>
-  </main>
 </template>
